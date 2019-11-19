4924c9533e8e329abce924eeba5608053d355ccf
dimastrn <adimassastranugraha@gmail.com>
2019-11-19 14:25:04 +0700

    UPDATE: kerjakan nanti

--- public/js/nav.js
+++ public/js/nav.js
@@ -20,3 +20,26 @@ document.addEventListener("DOMContentLoaded", function() {
       xhttp.send();
     }
   });
+
+  var page = window.location.hash.substr(1);
+
+  if(page == "") page = "home";
+  loadPage(page);
+
+  function loadPage(page) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if(this.readyState == 4) {
+        var content = document.querySelector('#body-content');
+        if(this.status == 200) {
+          content.innerHTML = xhttp.responseText;
+        } else if(this.status == 404) {
+          content.innerHTML == "<p>Page Not Found";
+        } else {
+          content.innerHTML = "Cant Access the page";
+        }
+      }
+    };
+    xhttp.open("GET", "pages/" + page + ".html", true);
+    xhttp.send();
+  }
